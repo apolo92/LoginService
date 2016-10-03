51a4ff46a5312cd6897e61e7cd865b6bc6f22b5b
Alejandro Polo Rangel <alejandro.polo@ext.aqualogybs.net>
2016-10-03 17:43:13 +0200

    Testing ok

--- src/test/java/com/test/web/domain/controller/ApiRestControllerTest.java
+++ src/test/java/com/test/web/domain/controller/ApiRestControllerTest.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import com.sun.net.httpserver.HttpExchange;
 import com.test.web.domain.factory.Factory;
 import com.test.web.domain.model.UserLogin;
+import com.test.web.domain.model.constants.Roles;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,6 +14,10 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by apolo on 03/10/2016.
@@ -38,8 +43,11 @@ public class ApiRestControllerTest {
     }
 
     @Test
-    public void insertNewUserByApiRest() throws IOException {
+    public void insertNewUserByApiRest() throws IOException, URISyntaxException {
         Mockito.when(http.getRequestMethod()).thenReturn("POST");
+
+        URI uri = new URI("http://localhost:8000/users?"+getValidJWT());
+        Mockito.when(http.getRequestURI()).thenReturn(uri);
 
         ApiRestController.users(http);
 
@@ -47,9 +55,13 @@ public class ApiRestControllerTest {
     }
 
     @Test
-    public void updateUserByApiRest() throws IOException {
+    public void updateUserByApiRest() throws IOException, URISyntaxException {
         Mockito.when(http.getRequestMethod()).thenReturn("PUT");
         Factory.getRepositoryInstance().loadUser(new UserLogin("test","test"));
+
+        URI uri = new URI("http://localhost:8000/users?"+getValidJWT());
+        Mockito.when(http.getRequestURI()).thenReturn(uri);
+
 
         ApiRestController.users(http);
 
@@ -62,5 +74,19 @@ public class ApiRestControllerTest {
         stub.addProperty("password","test");
         stub.addProperty("replaceUserName","test2");
         return new ByteArrayInputStream(stub.toString().getBytes());
+    }
+
+    private static String getValidJWT() {
+        UserLogin user = loadUserLogin();
+        String jwt = Factory.getLoginServiceInstance().generateJWT(user);
+        return jwt;
+    }
+
+    private static UserLogin loadUserLogin() {
+        List<Roles> role = new ArrayList<>();
+        role.add(Roles.ADMIN);
+        UserLogin user = new UserLogin("test3","test3",role);
+        Factory.getRepositoryInstance().loadUser(user);
+        return user;
     }
 }
